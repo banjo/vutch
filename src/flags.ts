3f8016d0f11049109a288110e379294d449f05cd
Anton Ödman <anton.odman@gmail.com>
2023-03-14 10:54:27 +0100

    chore: add check for files

--- src/flags.ts
+++ src/flags.ts
@@ -1,13 +1,26 @@
+import { globby } from "globby";
+import { ALWAYS_IGNORE } from "./config";
 import { Options } from "./types";
-import { callout, normal } from "./utils";
+import { callout, normal, warning } from "./utils";
 
-export const runFlags = (properties: Options) => {
-    const { files: flagFiles, command } = properties;
+export const runFlags = async (properties: Options) => {
+    const { files: flagFiles, command, deep } = properties;
 
     const files = flagFiles?.split(",");
 
     if (!files || !command) {
         throw new Error("You must provide both the files and the command");
+    }
+
+    const fileExists = await globby(files, {
+        gitignore: true,
+        deep: deep,
+        ignore: ALWAYS_IGNORE,
+    });
+
+    if (fileExists.length === 0) {
+        console.log(warning("No files found matching the files flag, exiting..."));
+        process.exit(0);
     }
 
     console.log(normal("\nVutch activated! Press Ctrl+C to stop watching"));
